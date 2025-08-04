ffb4cba4d4de550c236a2c6fda1b9d6756314c94
andrej <an.roesti@gmail.com>
2025-08-04 13:31:02 -0600

    add runlist evaluation

--- programming_examples/reconfig/run.sh
+++ programming_examples/reconfig/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-MODE=1
+set -e
+
+MODE=3
 ITERS=100
 
 if [[ $MODE -eq 0 ]] then
@@ -25,5 +27,33 @@ if [[ $MODE -eq 1 ]] then
     make build/npu_insts_main_rt.bin
     for i in $(seq $ITERS); do
         ./build/test ./build/empty.xclbin ./build/npu_insts_main_rt.bin
+    done
+fi
+
+
+# Individual configs and run insts with empty xclbin
+
+if [[ $MODE -eq 2 ]] then
+    make build/test
+    make build/empty.xclbin
+    make build/config_add_two_insts.bin
+    make build/run_add_two_insts.bin
+    make build/config_subtract_three_insts.bin
+    make build/run_subtract_three_insts.bin
+    for i in $(seq $ITERS); do
+        ./build/test ./build/empty.xclbin ./build/config_add_two_insts.bin ./build/empty.xclbin ./build/run_add_two_insts.bin ./build/empty.xclbin ./build/config_subtract_three_insts.bin ./build/empty.xclbin ./build/run_subtract_three_insts.bin
+    done
+fi
+
+# runlist
+
+if [[ $MODE -eq 3 ]] then
+    make build/test-runlist
+    make build/add_two.xclbin
+    make build/subtract_three.xclbin
+    make build/npu_insts_add_two_rt.bin
+    make build/npu_insts_subtract_three_rt.bin
+    for i in $(seq 0 $ITERS); do
+        ./build/test-runlist ./build/add_two.xclbin ./build/npu_insts_add_two_rt.bin ./build/subtract_three.xclbin ./build/npu_insts_subtract_three_rt.bin
     done
 fi
