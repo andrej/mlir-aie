145e495debcc9441f65deef7d3fee72e667ee0b1
andrej <an.roesti@gmail.com>
2025-08-07 17:24:32 -0600

    reset op insertion working, dead write elimination working; result slower than before due to all the 0 writes, some of which could probably be eliminated

--- lib/Dialect/AIEX/Transforms/AIEEliminateDeadWrite32s.cpp
+++ lib/Dialect/AIEX/Transforms/AIEEliminateDeadWrite32s.cpp
@@ -12,45 +12,41 @@
 #include "aie/Dialect/AIE/IR/AIETargetModel.h"
 #include "aie/Dialect/AIEX/IR/AIEXDialect.h"
 #include "aie/Dialect/AIEX/Transforms/AIEXPasses.h"
+#include "aie/Dialect/AIEX/AIEWrite32SequenceAnalysis.h"
 
 #include "mlir/Pass/Pass.h"
-#include "mlir/Transforms/DialectConversion.h"
-#include <algorithm>
-#include <cstdint>
+#include "mlir/Transforms/WalkPatternRewriteDriver.h"
+#include <iterator>
 
 using namespace mlir;
 using namespace xilinx;
 using namespace xilinx::AIEX;
 
-struct EliminateDeadWrite32sPattern : OpConversionPattern<NpuBlockWriteOp> {
-  using OpConversionPattern::OpConversionPattern;
-
-  EliminateDeadWrite32sPattern(MLIRContext *context, PatternBenefit benefit = 1)
-      : OpConversionPattern(context, benefit) {}
-
-  LogicalResult
-  matchAndRewrite(NpuBlockWriteOp op, OpAdaptor adaptor,
-                  ConversionPatternRewriter &rewriter) const override {
-    return failure();
-  }
-};
-
-
 struct AIEEliminateDeadWrite32sPass : AIEEliminateDeadWrite32sBase<AIEEliminateDeadWrite32sPass> {
   void runOnOperation() override {
     AIE::DeviceOp deviceOp = getOperation();
+    OpBuilder builder(deviceOp);
 
-    // target.addLegalDialect<AIEXDialect>();
-
-    ConversionTarget target(getContext());
-    target.addIllegalOp<NpuBlockWriteOp>();
-
-    RewritePatternSet patterns(&getContext());
-    patterns.insert<EliminateDeadWrite32sPattern>(&getContext());
-
-    if (failed(applyPartialConversion(deviceOp, target, std::move(patterns)))) {
-      signalPassFailure();
-    }
+    for (RuntimeSequenceOp runtimeSequenceOp : deviceOp.getOps<RuntimeSequenceOp>()) {
+      AnalysisManager am = getAnalysisManager().nest(runtimeSequenceOp);
+      AIEWrite32SequenceAnalysis writtenRegs = am.getAnalysis<AIEWrite32SequenceAnalysis>();
+      for (std::vector<NpuWrite32Op> &writeSequence : writtenRegs.writeSequences) {
+        // writes in writesequence are ordered by address, so it suffices to
+        // only compare neighboring writes
+        // iterate over all NpuWrite32Ops in the sequence, skipping the first one
+        for (auto it = writeSequence.begin(); it != writeSequence.end(); ++it) {
+          NpuWrite32Op &currentOp = *it;
+          auto nextOpIt = std::next(it);
+          if (nextOpIt != writeSequence.end()) {
+            NpuWrite32Op &nextOp = *nextOpIt;
+            // If the next write is to the same address, we can eliminate the current write
+            if (currentOp.getAbsoluteAddress() == nextOp.getAbsoluteAddress()) {
+              currentOp.erase();
+            }
+          }
+        }
+      } // end for each write sequence
+    } // end for each runtime sequence
   }
 };
 
