5af97c7227299e9f9432bd02580c94d1f9ea88e3
andrej <an.roesti@gmail.com>
2025-08-18 14:24:36 -0600

    compare force preemption enabled/disabled

--- programming_examples/preemption/eval.py
+++ programming_examples/preemption/eval.py
@@ -19,13 +19,21 @@ def parse_output(path):
     assert len(times) == len(preemptions)
     return times, preemptions
 
-times_with_preemption, count_with_preemption = parse_output("results-aie-with-preemption.txt")
-times_without_preemption, count_without_preemption = parse_output("results-aie-without-preemption.txt")
+times_with_preemption, count_with_preemption = parse_output("results-aie-with-preemption-enable.txt")
+times_without_preemption, count_without_preemption = parse_output("results-aie-without-preemption-enable.txt")
+times_with_preemption_disabled, count_with_preemption_disabled = parse_output("results-aie-with-preemption-disable.txt")
+times_without_preemption_disabled, count_without_preemption_disabled = parse_output("results-aie-without-preemption-disable.txt")
 
 mean_time_with_preemption = np.mean(times_with_preemption)
 mean_time_without_preemption = np.mean(times_without_preemption)
 mean_count_with_preemption = np.mean(count_with_preemption)
 mean_count_without_preemption = np.mean(count_without_preemption)
+mean_time_with_preemption_disabled = np.mean(times_with_preemption_disabled)
+mean_time_without_preemption_disabled = np.mean(times_without_preemption_disabled)
+mean_count_with_preemption_disabled = np.mean(count_with_preemption_disabled)
+mean_count_without_preemption_disabled = np.mean(count_without_preemption_disabled)
+print(f"With forced preemption enabled:")
+print(f"----")
 print(f"Number of measurements:                           {len(times_without_preemption):6} / {len(times_with_preemption):6}")
 print(f"Average kernel execution time without preemption: {mean_time_without_preemption:6.0f} μs")
 print(f"Average kernel execution time with preemption:    {mean_time_with_preemption:6.0f} μs")
@@ -33,14 +41,31 @@ print(f"Difference:                                       {mean_time_with_preemp
 print(f"Number of layer preemptions without preempt op:   {mean_count_without_preemption:6.0f}")
 print(f"Number of layer preemptions with preempt op:      {mean_count_with_preemption:6.0f}")
 print(f"Difference:                                       {mean_count_with_preemption - mean_count_without_preemption:6.0f}")
+print()
+print(f"With forced preemption disabled:")
+print(f"----")
+print(f"Number of measurements:                           {len(times_without_preemption_disabled):6} / {len(times_with_preemption_disabled):6}")
+print(f"Average kernel execution time without preemption: {mean_time_without_preemption_disabled:6.0f} μs")
+print(f"Average kernel execution time with preemption:    {mean_time_with_preemption_disabled:6.0f} μs")
+print(f"Difference:                                       {mean_time_with_preemption_disabled - mean_time_without_preemption_disabled:6.0f} μs")
+print(f"Number of layer preemptions without preempt op:   {mean_count_without_preemption_disabled:6.0f}")
+print(f"Number of layer preemptions with preempt op:      {mean_count_with_preemption_disabled:6.0f}")
+print(f"Difference:                                       {mean_count_with_preemption_disabled - mean_count_without_preemption_disabled:6.0f}")
+
+bars = [
+    ("Without preempt op", times_without_preemption_disabled),
+    ("With preempt op", times_with_preemption_disabled),
+    ("Without preempt op,\nforced preemption", times_without_preemption),
+    ("With preempt op,\nforced preemption", times_with_preemption)
+]
 
 fig, ax = plt.subplots()
-ax.bar(0, mean_time_without_preemption, label="Without Preemption")
-ax.boxplot(positions=[0], x=times_without_preemption, medianprops={"color":"black"})
-ax.bar(1, mean_time_with_preemption, label="With Preemption")
-ax.boxplot(positions=[1], x=times_with_preemption, medianprops={"color":"black"})
-ax.set_ylabel("Kernel Execution Time (μs)")
-ax.set_title("Kernel Execution Time With and Without Preemption")
-ax.set_xticks([0, 1])
-ax.set_xticklabels(["Without Preemption", "With Preemption"])
+for i, (label, values) in enumerate(bars):
+    ax.bar(i, np.mean(values), label=label)
+    ax.boxplot(positions=[i], x=values, medianprops={"color":"black"})
+    ax.set_ylabel("Kernel Execution Time (μs)")
+    ax.set_title("Kernel Execution Time With and Without Preemption")
+
+ax.legend(ncols=2)
+ax.set_xticks([])
 plt.savefig("eval.png")
--- programming_examples/preemption/run.sh
+++ programming_examples/preemption/run.sh
@@ -9,21 +9,23 @@ make clean
 make build/test
 
 for STEM in $STEMS; do
+    for FORCE_PREEMPT in "enable" "disable"; do
 
-    make build/$STEM.xclbin
+        make build/$STEM.xclbin
 
-    sudo /opt/xilinx/xrt/bin/xrt-smi configure --advanced --force-preemption --type layer --action enable
-    sudo /opt/xilinx/xrt/bin/xrt-smi configure --advanced --force-preemption --type frame --action enable
+        sudo /opt/xilinx/xrt/bin/xrt-smi configure --advanced --force-preemption --type layer --action $FORCE_PREEMPT
+        sudo /opt/xilinx/xrt/bin/xrt-smi configure --advanced --force-preemption --type frame --action $FORCE_PREEMPT
 
-    echo "" > results-$STEM.txt
-    for i in $(seq $ITERS); do
-        ./build/test -k MLIR_AIE -x build/$STEM.xclbin -i build/$STEM.elf | tee -a results-$STEM.txt
-        xrt-smi examine --advanced --report preemption | tee -a results-$STEM.txt
+        echo "" > results-$STEM-$FORCE_PREEMPT.txt
+        for i in $(seq $ITERS); do
+            ./build/test -k MLIR_AIE -x build/$STEM.xclbin -i build/$STEM.elf | tee -a results-$STEM-$FORCE_PREEMPT.txt
+            xrt-smi examine --advanced --report preemption | tee -a results-$STEM-$FORCE_PREEMPT.txt
+        done
+
+        sudo /opt/xilinx/xrt/bin/xrt-smi configure --advanced --force-preemption --type layer --action disable
+        sudo /opt/xilinx/xrt/bin/xrt-smi configure --advanced --force-preemption --type frame --action disable
+
     done
-
-    sudo /opt/xilinx/xrt/bin/xrt-smi configure --advanced --force-preemption --type layer --action disable
-    sudo /opt/xilinx/xrt/bin/xrt-smi configure --advanced --force-preemption --type frame --action disable
-
 done
 
 python3 ./eval.py
